39e8d1112f923ffbd636641ac1bc5d5d27b098ee
Rajat Gupta <rajat.gupta712@gmail.com>
2016-03-26 15:59:18 +0530

    Fixed bugs and added more scripts
    * Changed all scripts to executables
    * Fixed message bombarding on Slack by Hubot during deplyments
    * Added command to configure AWS Infra

--- bash/deploy-aws-infra.sh
+++ bash/deploy-aws-infra.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -e
+
 sudo su
 cd /bial
-ansible-playbook -i inventory/ec2.py --vault-password-file=/etc/ansible/.vault_password.txt -e "env=UAT" setup-aws-uat.yml
+
+echo 'Hey Dude.!! Go for Juice. I am setting up your AWS Infra. :blush: '
+
+ansible-playbook -i inventory/ec2.py --vault-password-file=/etc/ansible/.vault_password.txt -e "env=UAT" setup-aws-uat.yml > '/var/log/log.txt';
+
+echo 'Dude.!! Your AWS Infra is ready. :blush:'
+
+exit 0;
