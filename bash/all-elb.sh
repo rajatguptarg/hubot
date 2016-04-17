1a533dc889331a78be72fce81da9a708c71e7122
Rajat Gupta <rajat.gupta712@gmail.com>
2016-04-17 14:50:04 +0530

    Fixed issues and added notification script

--- bash/all-elb.sh
+++ bash/all-elb.sh
@@ -12,48 +12,48 @@ set -e
 cd ~/Projects/hubot/logs
 
 echo "\`Status for FlightInfo Service\`\n"
-echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name ELB-flightInfoService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
+echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name UAT-ELB-flightInfoService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
 cat elb_log.txt
 sleep 1s;
 
 
 echo "\`Status for User Service\`\n"
-echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name ELB-userService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
+echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name UAT-ELB-userService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
 cat elb_log.txt
 sleep 1s;
 
 echo "\`Status for Commercial Service\`\n"
-echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name ELB-commercialService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
+echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name UAT-ELB-commercialService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
 cat elb_log.txt
 sleep 1s;
 
 echo "\`Status for Notification Service\`\n"
-echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name ELB-notificationService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
+echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name UAT-ELB-notificationService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
 cat elb_log.txt
 sleep 1s;
 
 echo "\`Status for Location Service\`\n"
-echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name ELB-locationService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
+echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name UAT-ELB-locationService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
 cat elb_log.txt
 sleep 1s;
 
 echo "\`Status for Contextual Service\`\n"
-echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name ELB-contextualService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
+echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name UAT-ELB-contextualService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
 cat elb_log.txt
 sleep 1s;
 
 echo "\`Status for Content Management Service\`\n"
-echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name ELB-contentManagementService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
+echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name UAT-ELB-contentManagementService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
 cat elb_log.txt
 sleep 1s;
 
 echo "\`Status for Weather Service\`\n"
-echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name ELB-weatherService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
+echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name UAT-ELB-weatherService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
 cat elb_log.txt
 sleep 1s;
 
 echo "\`Status for Feedback Service\`\n"
-echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name ELB-feedbackService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
+echo "\`\`\`" | (aws elb describe-instance-health --load-balancer-name UAT-ELB-feedbackService --output table| (echo "\`\`\`" && cat) && cat) > elb_log.txt
 cat elb_log.txt
 sleep 1s;
 
---
+++ handlers/notify
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is machine specific configurations
+
+set -e
+
+SCRIPT_PATH=/Users/rajatg/Projects/hubot/bash
+
+cd /Users/rajatg/pyProjects/offers/infra
+
+echo "\`Started looking for the ELB Healths\`\n"
+
+sleep 1s;
+
+bash $SCRIPT_PATH/notify.sh
