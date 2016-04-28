78c2c6d82470205bb2b1081290007092d5f43a51
Rajat Gupta <rajat.gupta712@gmail.com>
2016-04-28 19:39:19 +0530

    Pulling latest code before deploying

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 
 while true
 do
+    git pull --rebase
     bin/hubot -a slack -n samantha >> hubot.log
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SCRIPT RESTARTED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" >> hubot.log
 done
