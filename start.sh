7a6fa68e82976160ef6e5be3b01e3b00e1f3d811
Rajat Gupta <rajatguptarg@users.noreply.github.com>
2016-05-09 20:30:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 while true
 do
-    git pull --rebase
+    git pull --rebase origin master
     bin/hubot -a slack -n samantha >> hubot.log
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SCRIPT RESTARTED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" >> hubot.log
 done
