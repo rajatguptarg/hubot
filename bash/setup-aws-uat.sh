ea3cfef7707aa36648af596e56c4031a8ea23aa3
Rajat Gupta <rajat.gupta712@gmail.com>
2016-05-22 18:40:48 +0530

    Fixed script to deploy AWS

--- bash/setup-aws-uat.sh
+++ bash/setup-aws-uat.sh
@@ -6,4 +6,4 @@ sudo su
 cd /bial
 echo 'Hey Dude.!! Go for Juice. I am setting up the AWS Infrastructure..!! :upside_down_face:'
 
-ansible-playbook -i inventory/ec2.py --vault-password-file=/etc/ansible/.vault_password.txt -e "env=UAT" setup-aws-uat.yml > '/var/log/log.txt' && echo 'Your AWS Infra is ready. :blush:' && exit 0;
+ansible-playbook setup-aws-uat.yml > '/var/log/log.txt' && echo 'Your AWS Infra is ready. :blush:' && exit 0;
